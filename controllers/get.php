abb0beafe0b3c5e1b3184c64d4141cf2f0087fff
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-03-15 22:35:56 +0000

    Implemented get controller

--- controllers/get.php
+++ controllers/get.php
@@ -11,4 +11,22 @@
  * @copyright Sunnefa Lind 2013
  * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License 3.0
  */
+
+$queries = Functions::query_strings();
+
+if(!empty($queries[0])) {
+    $page_slug = $queries[0];
+} else {
+    $page_slug = $settings->home_page_slug;
+}
+
+try {
+    $page = new Page($sql, $page_slug);
+    foreach($page->page_modules as $display_order => $module) {
+        include MODULES . $module->module_path;
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+    //@todo: Redirect to 404 here
+}
 ?>
