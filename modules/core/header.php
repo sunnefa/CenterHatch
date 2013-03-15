a130de4a9c0e7b3010883e522f4dcd1083a3181a
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-03-15 22:40:18 +0000

    Created header and footer modules

---
+++ modules/core/header.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *  A short description of this file
+ * 
+ *  A longer description of this file
+ * 
+ * @filename header.php
+ * @package 
+ * @version 0.1
+ * @author Sunnefa Lind <sunnefa_lind@hotmail.com>
+ * @copyright Sunnefa Lind 2013
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License 3.0
+ */
+
+echo 'This is header.php';
+?>
