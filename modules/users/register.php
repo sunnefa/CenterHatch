49c41f6e291f27c6d86e7596abbfff6b5e5ec734
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-09-23 00:15:05 +0000

    Created User modules
    Account, activate, login, logout, password, register

---
+++ modules/users/register.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *  The controller for registering a user
+ * 
+ *  This is the controller for user registration, it handles both the showing of the form and the
+ *  neccessary object instantiations and function calls. It ends with the user being sent an email to activate
+ *  their account
+ * 
+ * @filename modules/users/register.php
+ * @package Users
+ * @version 0.1
+ * @author Sunnefa Lind <sunnefa_lind@hotmail.com>
+ * @copyright Sunnefa Lind 2013
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License 3.0
+ */
+
+if(!isset($_POST['register'])) {
+    //show the form
+} else {
+    //validate the data and register the user
+}
+?>
